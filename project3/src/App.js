39c2be39e4acc6ebe66bb943576e16737f1c7e12
Andrew Kerr <andrewkerr3956@gmail.com>
2021-09-28 22:06:27 -0400

    Began building the UI for the app.

--- project3/src/App.js
+++ project3/src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+// import {Link, Route, BrowserRouter} from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>To-Do List</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h2>React + Redux To-Do App</h2>
+        <Navbar />
       </header>
     </div>
   );
