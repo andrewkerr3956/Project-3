3fb1c2d1bcabb47fd41ef0f612dc983f46f76b2c
Andrew Kerr <andrewkerr3956@gmail.com>
2021-09-29 21:17:11 -0400

    Fixed up the navbar, added the react-router-dom and redux libraries, and added React Router functionality to the two pages.

--- project3/src/App.js
+++ project3/src/App.js
@@ -1,25 +1,50 @@
 import './App.css';
-// import {Link, Route, BrowserRouter} from 'react-router-dom';
+import React from 'react';
+import {Link, Route, BrowserRouter} from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav>
       <ul>
-        <li>To-Do List</li>
-        <li>Contact</li>
+        {/* Add React Router functionality later on */}
+        <li><Link to="/todos">To-Do List</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
       </ul>
     </nav>
-  )
+  );
 }
+
+const ToDoList = () => {
+  return (
+    <div className="container">
+      <div className="flex">
+        
+      </div>
+    </div>
+  )
+} 
 
 function App() {
   return (
+    <BrowserRouter>
     <div>
       <header>
         <h2>React + Redux To-Do App</h2>
         <Navbar />
       </header>
+      <Route path="/todos">
+        <div className="form-container">
+          <form>
+            <input type="text" name="task-input" placeholder="Enter task..." />
+            <button>Add Task</button>
+          </form>
+        </div>
+        <ToDoList />
+      </Route>
     </div>
+    
+    <Route path="/contact" />
+    </BrowserRouter>
   );
 }
 
