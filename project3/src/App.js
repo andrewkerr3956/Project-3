abcbd8bb84889cbecc114ed5c72a6afbc94e26fa
Andrew Kerr <andrewkerr3956@gmail.com>
2021-09-29 23:58:50 -0400

    Added Bootstrap, changed the Links to NavLinks in the navbar, fixed an issue with the React Router and Route paths, and also, created a basic design for the todos and contact pages.

--- project3/src/App.js
+++ project3/src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import React from 'react';
-import {Link, Route, BrowserRouter} from 'react-router-dom';
+import { NavLink, Link, Route, BrowserRouter } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav>
       <ul>
         {/* Add React Router functionality later on */}
-        <li><Link to="/todos">To-Do List</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+        <li><NavLink to="/todos">To-Do List</NavLink></li>
+        <li><NavLink to="/contact">Contact</NavLink></li>
       </ul>
     </nav>
   );
@@ -17,34 +17,64 @@ const Navbar = () => {
 const ToDoList = () => {
   return (
     <div className="container">
-      <div className="flex">
-        
+      <div className="todo-container">
+        <h2>Add items to your To-Do list!</h2>
       </div>
     </div>
   )
-} 
+}
+
+const ToDoContent = () => {
+  return (
+    <div className="task-container">
+      <input type="text" name="task" placeholder="Enter task..." />
+      <button>Add Task</button>
+      <ToDoList />
+    </div>
+    /*
+    <div className="container">
+      <div className="flex">
+
+      </div>
+    </div>
+    */
+  )
+}
+
+const ContactContent = () => {
+  return (
+    <div className="container">
+      <div className="form-container">
+        <form className="container-fluid">
+          <label for="firstName">First Name</label> <br />
+          <input type="text" name="firstName" />
+          <p />
+          <label for="lastName">Last Name</label> <br />
+          <input type="text" name="lastName" />
+          <p />
+          <label for="email">E-Mail Address</label> <br />
+          <input type="email" name="email" />
+          <p />
+          <label for="comments">Comments</label> <br />
+          <textarea></textarea>
+          <p />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
+  )
+}
 
 function App() {
   return (
     <BrowserRouter>
-    <div>
       <header>
         <h2>React + Redux To-Do App</h2>
         <Navbar />
       </header>
-      <Route path="/todos">
-        <div className="form-container">
-          <form>
-            <input type="text" name="task-input" placeholder="Enter task..." />
-            <button>Add Task</button>
-          </form>
-        </div>
-        <ToDoList />
-      </Route>
-    </div>
-    
-    <Route path="/contact" />
-    </BrowserRouter>
+      <Route path="/todos" component={ToDoContent} />
+      <Route path="/contact" component={ContactContent} />
+    </BrowserRouter >
   );
 }
 
