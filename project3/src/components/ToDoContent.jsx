2aea962012373e0728a65472198025cf52ae5305
Andrew Kerr <andrewkerr3956@gmail.com>
2021-10-03 23:37:54 -0400

    You can now delete tasks from the To-Do list. The filter views will have their functionality added very soon. Minor CSS adjustments.

--- project3/src/components/ToDoContent.jsx
+++ project3/src/components/ToDoContent.jsx
@@ -5,13 +5,41 @@ const ToDoContent = () => {
 
   const [taskName, setTaskName] = useState("");
   const [tasks, setTasks] = useState([]);
+  const [active, setActive] = useState(true);
 
   const updateTaskName = (event) => {
     setTaskName(event.target.value);
   }
 
+  const deleteTask = (event) => {
+    let newListTasks = [];
+    tasks.map( (task, idx) => {
+      if( idx != event.target.id) {
+        newListTasks.push(task);
+      } 
+    });
+    setTasks(newListTasks);
+  }
+
+  /*
+  const updateActive = (event) => {
+    if(event.target.active) {
+      setActive(false);
+    }
+    else {
+      setActive(true);
+    }
+  }
+  */
+
   const updateList = () => {
-    setTasks([...tasks, taskName]);
+      if(tasks.includes(taskName)) {
+        alert("Task already exists!");
+      }
+      else {
+        setTasks([...tasks, taskName]);
+      }
+      
   }
 
   return (
@@ -19,12 +47,20 @@ const ToDoContent = () => {
       {/* <TaskListContainer /> */}
       <input type="text" name="task" value={taskName} onChange={updateTaskName} placeholder={"Enter task..."} />
       <button onClick={updateList}>Add Task</button> <br />
-      <div className="container-fluid" style={{width: "500px", marginTop: "30px", display: "flex", background: "gray"}}>
-        <ul>
+      <div className={"container-fluid"} style={{width: "500px", marginTop: "30px", display: "flex", flexFlow: "wrap row", background: "white"}}>
         {tasks && tasks.length > 0 && tasks.map( (task, idx) => {
-          return <li key={idx} style={{flexBasis: "100%"}}>{task}</li>
+          return (
+            <ul style={{justifyContent: "space-between", textAlign: "left", listStyleType: "none", background: "gray", borderBottom: "2px solid rgba(0,0,0,0.8)", width: "500px", padding: "15px"}}>
+              <li key={idx}>
+                <input type="checkbox" id={idx} className="flex-item"/>
+                <span className="flex-item"><strong>{task}</strong></span>
+                <button id={idx} onClick={deleteTask}>X</button>
+              </li>
+              </ul>
+          )
+          
         })}
-        </ul>
+        
       </div>
       <div style={{marginTop: "50px"}}>
         <button>Show All</button>
